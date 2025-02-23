e269a1e657bd69e4bbda96c68c02cca1725fcaee
aklein <aklein@student.hive.fi>
2025-02-23 19:02:57 +0200

    feat: small testing server

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include "server/server.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <iostream>
 
 int main() {
     std::cout << "Hello" << std::endl;
-    return 0;
+
+    Server myserver = Server(6667);
+
+    myserver.start();
 }
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,7 +1,29 @@
 #include "server.h"
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <iostream>
+
 Server::Server(int port) : port(port), server_fd(-1) {
 }
 
 void Server::start() {
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(this->port);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    bind(server_fd, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+    listen(server_fd, -1);
+
+    int clientSocket = accept(server_fd, nullptr, nullptr);
+
+    char buffer[1024] = {0};
+    recv(clientSocket, buffer, sizeof(buffer), 0);
+
+    std::cout << "message from client: " << buffer << std::endl;
+    close(server_fd);
 }
