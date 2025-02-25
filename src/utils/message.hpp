bce40a25f202df8e57e914c7177fa8d906d1a39c
Leonor Tu <jingyi.tu@outlook.com>
2025-02-25 15:16:29 +0200

    feat: create message struct and parse function

--- src/utils/message.hpp
+++ src/utils/message.hpp
@@ -1,3 +1,12 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <sstream>
+
+struct message {
+    std::string prefix;
+    std::string command;
+    std::vector<std::string> parameters;
+	int parseCommand(const std::string &rawMessage);
+};
