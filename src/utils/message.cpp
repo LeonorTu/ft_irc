6c1dec081096b633963fd0e762451fc0c8f3f19d
Leonor Tu <jingyi.tu@outlook.com>
2025-02-25 15:16:29 +0200

    feat: create message struct and parse function

--- src/utils/message.cpp
+++ src/utils/message.cpp
@@ -1,1 +1,30 @@
 #include "message.hpp"
+
+int message::parseCommand(const std::string &rawMessage)
+{
+    if (rawMessage.empty())
+        return (0);
+
+    std::istringstream stream(rawMessage);
+    std::string word;
+
+    // prefix
+    if (rawMessage[0] == ':') {
+        stream >> prefix;
+        prefix = prefix.substr(1);
+    }
+
+    // command
+    if (stream >> command) {
+        // parameters
+        while (stream >> word) {
+            if (word[0] == ':') {
+                std::string trailing;
+                std::getline(stream, trailing);
+                parameters.push_back(word.substr(1) + trailing);
+                break;
+            }
+            parameters.push_back(word);
+        }
+    }
+}
