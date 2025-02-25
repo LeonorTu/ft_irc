1cea6043a23ef71fc87a7de141f9f2ca3865dcf4
aklein <aklein@student.hive.fi>
2025-02-25 23:07:10 +0200

    tests:added some stupid tests for samples of how they could work

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,17 +1,94 @@
 #include <gtest/gtest.h>
 #include <server.hpp>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <thread>
+#include <chrono>
 
-TEST(ServerTest, InitializationTest)
+class ServerTest : public ::testing::Test {
+protected:
+    Server *server; // Shared resource for all tests
+
+    void SetUp() override
+    {
+        // Runs before each test
+        server = new Server();
+    }
+
+    void TearDown() override
+    {
+        // Runs after each test
+        delete server;
+    }
+};
+
+// Existing test
+TEST_F(ServerTest, InitializationTest)
 {
-    Server server;
-    EXPECT_EQ(server.getPort(), 6667); // Assuming default IRC port
-    EXPECT_EQ(server.getServerFD(), -1);
+    EXPECT_EQ(server->getPort(), 6667);
+    EXPECT_EQ(server->getServerFD(), -1);
 }
 
-// TEST(ServerTest, StartServerTest)
-// {
-//     Server server;
-//     server.start();
-//     EXPECT_GE(server.getServerFD(), 0);
-//     server.stop();
-// }
+TEST_F(ServerTest, StartStopTest)
+{
+    std::thread server_thread([this]() { this->server->start(); });
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    this->server->stop();
+    server_thread.join();
+    EXPECT_FALSE(this->server->paused);
+}
+
+TEST_F(ServerTest, WelcomeMessageTest)
+{
+    std::thread server_thread([this]() { this->server->start(); });
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    // Use a short timeout to close automatically
+    FILE *nc = popen("echo '' | nc -w 1 localhost 6667", "r");
+    ASSERT_NE(nc, nullptr);
+
+    char buffer[1024] = {0};
+    std::string output;
+    while (fgets(buffer, sizeof(buffer), nc) != nullptr) {
+        output.append(buffer);
+    }
+    std::cout << output << std::endl;
+
+    // Check welcome message components
+    EXPECT_NE(output.find("001"), std::string::npos);
+    EXPECT_NE(output.find("002"), std::string::npos);
+    EXPECT_NE(output.find("003"), std::string::npos);
+    EXPECT_NE(output.find("004"), std::string::npos);
+
+    pclose(nc);
+
+    this->server->stop();
+    server_thread.join();
+}
+
+TEST_F(ServerTest, PauseResumeTest)
+{
+    EXPECT_FALSE(server->paused);
+    server->pause();
+    EXPECT_TRUE(server->paused);
+    server->resume();
+    EXPECT_FALSE(server->paused);
+}
+
+TEST_F(ServerTest, MessageSizeLimitTest)
+{
+    std::thread server_thread([this]() { this->server->start(); });
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    // Create an oversized message
+    std::string large_message(600, 'A');
+
+    FILE *nc = popen(("echo '" + large_message + "' | nc localhost 6667").c_str(), "r");
+    ASSERT_NE(nc, nullptr);
+    pclose(nc);
+
+    this->server->stop();
+    server_thread.join();
+}
