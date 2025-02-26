1d5a076fcc523d1b5fe8e72490bfac8da469e1d9
Leonor Tu <jingyi.tu@outlook.com>
2025-02-26 20:00:34 +0200

    feat: enhance message structure with command handling and add new command files

--- include/message.hpp
+++ include/message.hpp
@@ -3,10 +3,13 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <unordered_map>
+#include <functional>
 
 struct message {
     std::string prefix;
     std::string command;
     std::vector<std::string> parameters;
     message(const std::string &rawMessage);
+    std::unordered_map<std::string, std::function<void(void)>> commands;
 };
