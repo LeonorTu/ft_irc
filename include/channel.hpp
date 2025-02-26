53fa7b5dc8125a19328f77aee196a3d19f266736
aklein <aklein@student.hive.fi>
2025-02-26 19:32:34 +0200

    feat: implement Channel class with join, leave, and mode management functionalities

--- include/channel.hpp
+++ include/channel.hpp
@@ -1,16 +1,29 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
 
 class Client;
+class Server;
 
 class Channel {
 public:
-    Channel(const std::string &name);
-    void addClient(Client *client);
-    void removeClient(Client *client);
-    void sendMessageToChannel(const std::string &message);
+    Channel();
+    Channel(const std::string &name, Client &creator);
+    void join(Client &client);
+    void leave(Client &client);
+    void message(const std::string &message);
+    void toggleMode(char mode);
+
+    bool hasMode(const char mode) const;
 
 private:
     std::string name;
+    std::string topic;
+    std::unordered_map<int, Client &> connectedClients;
+    std::unordered_map<int, Client &> ops;
+    // itkl
+    std::string modes;
+    std::string key;
+    int userLimit;
 };
--- src/channel/channel.cpp
+++ src/channel/channel.cpp
@@ -1,4 +1,48 @@
 #include <channel.hpp>
+#include <server.hpp>
+#include <Client.hpp>
 
-Channel::Channel(const std::string &name) : name(name) {
+Channel::Channel()
+    : name("Default")
+    , modes("")
+    , key("")
+    , userLimit(0)
+{
+}
+
+Channel::Channel(const std::string &name, Client &creator)
+    : name(name)
+    , modes("")
+    , key("")
+    , userLimit(0)
+{
+    connectedClients[creator.getFd()] = creator;
+    ops[creator.getFd()] = creator;
+}
+
+void Channel::join(Client &client)
+{
+    connectedClients[client.getFd()] = client;
+}
+
+void Channel::leave(Client &client)
+{
+    connectedClients.erase(client.getFd());
+    ops.erase(client.getFd());
+}
+
+void Channel::toggleMode(char mode)
+{
+    size_t index = modes.find(mode);
+    if (index == std::string::npos) {
+        modes.push_back(mode);
+    }
+    else {
+        modes.erase(index);
+    }
+}
+
+bool Channel::hasMode(const char mode) const
+{
+    return modes.find(mode) != std::string::npos;
 }
