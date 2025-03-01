6073ef2aba22438d3bd43a744198bcc76459c911
aklein <aklein@student.hive.fi>
2025-03-01 15:36:07 +0200

    feat: add maximum length constants for channel names and nicknames, update user and channel modes

--- include/common.hpp
+++ include/common.hpp
@@ -6,10 +6,13 @@ const int MAX_CLIENTS = 100;     // Maximum number of clients
 const int MSG_BUFFER_SIZE = 512; // Buffer size for message handling
 const int SERVER_PORT = 6667;    // Server port
 
+const int CHANNEL_NAME_MAX = 50;
+const int NICKNAME_MAX = 512;
+
 const std::string SERVER_NAME = "localhost";
 const std::string NETWORK_NAME = "J-A";
 const std::string SERVER_VERSION = "v1.0";
-const std::string USER_MODES = "itkol";
-const std::string CHANNEL_MODES = "bklov";
+const std::string USER_MODES = "o";
+const std::string CHANNEL_MODES = "itkl";
 
 const int EPOLL_MAX_EVENTS = 10;
