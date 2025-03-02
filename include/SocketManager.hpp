f003aa6e5192785e8b6d37fdb13b844fad150191
aklein <aklein@student.hive.fi>
2025-03-02 23:23:14 +0200

    feat: implement SocketManager class for handling socket connections

---
+++ include/SocketManager.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <iostream>
+#include <cstring>
+
+class SocketManager
+{
+public:
+    SocketManager(int port);
+    ~SocketManager();
+
+    int initialize();
+    void closeAllConnections();
+    int acceptConnection(sockaddr_in *clientAddr);
+
+private:
+    int _serverFd;
+    int _port;
+    sockaddr_in _serverAddress;
+};
