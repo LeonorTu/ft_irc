2f1040cac1ae3f54d9be3f4c15be1081ff973389
aklein <aklein@student.hive.fi>
2025-03-13 00:26:48 +0200

    feat: add ChannelManager class for managing channels and their lifecycle

---
+++ include/ChannelManager.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <memory>
+#include <unordered_map>
+
+class Channel;
+class Client;
+
+class ChannelManager
+{
+public:
+    ChannelManager();
+    ~ChannelManager();
+
+    bool channelExists(const std::string &name) const;
+    void createChannel(const std::string &name, Client &creator);
+    void removeChannel(const std::string &name);
+    Channel &getChannel(const std::string &name) const;
+
+private:
+    std::unordered_map<std::string, std::unique_ptr<Channel>> _channels;
+
+    // ascii casemapping, Defines the characters a to z
+    // to be considered the lower-case equivalents of the characters A to Z only.
+    std::string caseMapped(const std::string &name) const;
+};
