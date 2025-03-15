bced12b1d79f5328f4db7344ff0b4cf8670d65dc
Leonor Tu <jingyi.tu@outlook.com>
2025-03-15 20:41:25 +0200

    feat: add join, part, and quit command handlers to command processor header

--- include/commandHandlers.hpp
+++ include/commandHandlers.hpp
@@ -6,4 +6,7 @@ void nick(const CommandProcessor::CommandContext &ctx);
 void pass(const CommandProcessor::CommandContext &ctx);
 void user(const CommandProcessor::CommandContext &ctx);
 void silentIgnore(const CommandProcessor::CommandContext &ctx);
+void join(const CommandProcessor::CommandContext &ctx);
+void part(const CommandProcessor::CommandContext &ctx);
+void quit(const CommandProcessor::CommandContext &ctx);
 void sendWelcome(int clientFd);
