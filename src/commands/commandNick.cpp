32ce4bfa53a3b450cdd1a7aa8e533084dbb43953
aklein <aklein@student.hive.fi>
2025-03-17 23:10:03 +0200

    feat: add nickname existence check in CommandRunner

--- src/commands/commandNick.cpp
+++ src/commands/commandNick.cpp
@@ -8,4 +8,11 @@ void CommandRunner::nick()
 
     std::string &oldNickname = _nickname;
     std::string &newNickname = _params[0];
+
+    if (_clients.nickExists(newNickname))
+        sendToClient(_clientFd, ERR_NICKNAMEINUSE(oldNickname, newNickname));
+
+    _client.setNickname(newNickname);
+    if (tryRegisterClient())
+        _clients.addNick(_clientFd);
 }
