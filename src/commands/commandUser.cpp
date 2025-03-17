dc00cc56dab51655839692866c626d95d6416a23
aklein <aklein@student.hive.fi>
2025-03-17 23:10:10 +0200

    feat: implement user command handling in CommandRunner

---
+++ src/commands/commandUser.cpp
@@ -0,0 +1,21 @@
+#include <CommandRunner.hpp>
+
+void CommandRunner::user()
+{
+    std::array<ParamType, MAX_PARAMS> pattern = {USER, NOVAL, NOVAL, REAL};
+    if (!validateParams(4, 4, pattern))
+        return;
+
+    if (_client.getIsRegistered()) {
+        sendToClient(_clientFd, ERR_ALREADYREGISTERED(_nickname));
+        return;
+    }
+
+    std::string username = _params[0];
+    std::string realname = _params[3];
+    _client.setUsername(username);
+    _client.setRealname(realname);
+
+    if (tryRegisterClient())
+        _clients.addNick(_clientFd);
+}
