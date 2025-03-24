097eb94f0275b7b32f22d6dba26d915b4c641c21
Leonor Tu <jingyi.tu@outlook.com>
2025-03-24 15:52:27 +0200

    feat: use parameter validation for part command

--- src/commands/part.cpp
+++ src/commands/part.cpp
@@ -2,11 +2,10 @@
 
 void CommandRunner::part()
 {
-    if (_params.empty()) {
-        sendToClient(_clientFd, ERR_NEEDMOREPARAMS(_client.getNickname(), "PART"));
+    std::array<ParamType, MAX_PARAMS> pattern = {};
+    if (!validateParams(1, 2, pattern))
         return;
-    }
-
+        
     std::istringstream channelList(_params[0]);
     std::string channelName;
     std::string reason = _params.size() >= 2 ? _params[1] : "";
