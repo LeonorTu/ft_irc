68696c6d6e8aba6267e7cc76baf1a0855f02cce4
Leonor Tu <jingyi.tu@outlook.com>
2025-03-24 15:48:42 +0200

    feat: add parameter validation and channel existence check in kick command

--- src/commands/kick.cpp
+++ src/commands/kick.cpp
@@ -2,10 +2,18 @@
 
 void CommandRunner::kick()
 {
+    std::array<ParamType, MAX_PARAMS> pattern = {VAL_CHAN};
+    if (!validateParams(2, 3, pattern))
+        return;
     std::string channelName = _params[0];
     std::string targetNicknames = _params[1];
     std::string reason = (_params.size() > 2) ? _params[2] : "No reason given";
 
+    if (!_channels.channelExists(channelName))
+    {
+        sendToClient(_clientFd, ERR_NOSUCHCHANNEL(_nickname, channelName));
+        return;
+    }
     Channel &channel = _channels.getChannel(channelName);
     std::istringstream ss(targetNicknames);
     std::string targetNickname;
