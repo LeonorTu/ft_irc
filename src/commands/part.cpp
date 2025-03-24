37156bb903a1fe570238cc8954278b13fce02373
Leonor Tu <jingyi.tu@outlook.com>
2025-03-24 16:41:38 +0200

    feat: initialize pattern array with VAL_NONE in part command

--- src/commands/part.cpp
+++ src/commands/part.cpp
@@ -2,10 +2,10 @@
 
 void CommandRunner::part()
 {
-    std::array<ParamType, MAX_PARAMS> pattern = {};
+    std::array<ParamType, MAX_PARAMS> pattern = {VAL_NONE};
     if (!validateParams(1, 2, pattern))
         return;
-        
+
     std::istringstream channelList(_params[0]);
     std::string channelName;
     std::string reason = _params.size() >= 2 ? _params[1] : "";
