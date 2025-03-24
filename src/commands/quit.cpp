1fae0c58fc54c82e48580b3990fa3775efd7809b
aklein <aklein@student.hive.fi>
2025-03-24 03:28:17 +0200

    fix: correct capitalization in quit command disconnection message and comment out error throwing in epoll removal

--- src/commands/quit.cpp
+++ src/commands/quit.cpp
@@ -3,5 +3,5 @@
 void CommandRunner::quit()
 {
     std::string reason = _params.empty() ? "" : _params[0];
-    _server.getConnectionManager().disconnectClient(_client, "QUIT: " + reason);
+    _server.getConnectionManager().disconnectClient(_client, "Quit: " + reason);
 }
